106ebba94a6e79c32dc797913ae40ce20058821e
Mindaugas Barysas <mindaugas@barysas.eu>
2015-04-20 19:23:56 +0300

    API added

--- PHR-RDF-aggregator/PHRPluginVivaport/PHRPluginVivaport.cs
+++ PHR-RDF-aggregator/PHRPluginVivaport/PHRPluginVivaport.cs
@@ -1,10 +1,17 @@
-﻿namespace Vulsk.CarrePhrAggregator.PhrPlugins
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace Vulsk.CarrePhrAggregator.PhrPlugins
 {
 	using DataSpecification;
+	using PatientSummary;
 
 	public class PhrPluginVivaport : IPhrInput
 	{
-
 		private readonly SourceIdentifier _sourceId = new SourceIdentifier {
 			InternalId = typeof(PhrPluginVivaport).GUID,
 			SourceName = "Vivaport.eu"
@@ -13,9 +20,120 @@
 
 		public PhrData GetData(PatientIdentifier p)
 		{
-			var retData = new PhrData();
+			var ret = new PhrData { Data = new List<DataUnit>(), Patient = p, Source = Source };
 
-			return retData;
+			using (var client = new HttpClient()) {
+				client.BaseAddress = new Uri(ConfigurationManager.AppSettings["VivaportApiUri"]);
+				client.DefaultRequestHeaders.Accept.Clear();
+				client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+				//get patient personal information
+				var response = client.GetAsync(string.Format("api/PatientSummary/{0}", p.InternalId)).Result;
+				if (response.IsSuccessStatusCode) {
+					var persInfo = response.Content.ReadAsAsync<PersonalInformation>().Result;
+					ret.Data.Add(new DataUnit {
+						Datetime = DateTime.UtcNow,
+						Name = "Name",
+						OntologicName = "rdf:Name",
+						Value = persInfo.contactInfo.sName
+					});
+					ret.Data.Add(new DataUnit {
+						Datetime = persInfo.dtLastUpdate,
+						Name = "Birth date",
+						OntologicName = "rdf:BDate",
+						Value = persInfo.dtDateofBirth
+					});
+					ret.Data.Add(new DataUnit {
+						Datetime = persInfo.dtLastUpdate,
+						Name = "Gender",
+						OntologicName = "rdf:Gender",
+						Value = persInfo.sGender
+					});
+					ret.Data.Add(new DataUnit {
+						Datetime = persInfo.dtLastUpdate,
+						Name = "Country",
+						OntologicName = "rdf:Country",
+						Value = persInfo.ciMyContacts.FirstOrDefault(c => c.adress.sCountry != null)
+					});
+				}
+
+				//get diagnosis
+				response = client.GetAsync(string.Format("api/Diagnosis/{0}", p.InternalId)).Result;
+				if (response.IsSuccessStatusCode) {
+					var diagnosis = response.Content.ReadAsAsync<ProblemRCI>().Result;
+					ret.Data.Add(new DataUnit {
+						Datetime = diagnosis.dtDate,
+						Name = "ICD",
+						OntologicName = "rdf:ICD",
+						Value = diagnosis.sICD
+					});
+					ret.Data.Add(new DataUnit {
+						Datetime = diagnosis.dtDate,
+						Name = "Description",
+						OntologicName = "rdf:Description",
+						Value = diagnosis.sDiagnosisDescription
+					});
+					ret.Data.Add(new DataUnit {
+						Datetime = diagnosis.dtDate,
+						Name = "OnsetDate",
+						OntologicName = "rdf:OnsetDate",
+						Value = diagnosis.sOnsetDate
+					});
+					ret.Data.Add(new DataUnit {
+						Datetime = diagnosis.dtDate,
+						Name = "EndDate",
+						OntologicName = "rdf:EndDate",
+						Value = diagnosis.sendDate
+					});
+					ret.Data.Add(new DataUnit {
+						Datetime = diagnosis.dtDate,
+						Name = "ResolutionCircumstances",
+						OntologicName = "rdf:ResolutionCircumstances",
+						Value = diagnosis.sResolution
+					});
+				}
+
+				//get medication
+				response = client.GetAsync(string.Format("api/Diagnosis/{0}", p.InternalId)).Result;
+				if (response.IsSuccessStatusCode) {
+					var medication = response.Content.ReadAsAsync<Medication>().Result;
+					foreach (var m in medication.medicineList)
+					{
+						ret.Data.Add(new DataUnit {
+							Datetime = m.dtLastUpdate,
+							Name = "Medication",
+							OntologicName = "rdf:Medication",
+							Value = m.sActiveIngredient
+						});
+						ret.Data.Add(new DataUnit {
+							Datetime = m.dtLastUpdate,
+							Name = "Dose",
+							OntologicName = "rdf:Dose",
+							Value = m.sDose
+						});
+						ret.Data.Add(new DataUnit {
+							Datetime = m.dtLastUpdate,
+							Name = "TimesPerDay",
+							OntologicName = "rdf:TimesPerDay",
+							Value = m.sFrequency
+						});
+						ret.Data.Add(new DataUnit {
+							Datetime = m.dtLastUpdate,
+							Name = "StartDate",
+							OntologicName = "rdf:StartDate",
+							Value = m.dtStarted
+						});
+						ret.Data.Add(new DataUnit {
+							Datetime = m.dtLastUpdate,
+							Name = "EndDate",
+							OntologicName = "rdf:EndDate",
+							Value = m.dtEnded
+						});
+					}
+				}
+			}
+
+			return ret;
 		}
 	}
 }
--- PHR-RDF-aggregator/ResourceOutput/ResourceOutput.cs
+++ PHR-RDF-aggregator/ResourceOutput/ResourceOutput.cs
@@ -1,9 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Text;
 using System.Xml;
 using System.Xml.Xsl;
 using System.IO;
 using System.Reflection;
+using VDS.RDF;
+using VDS.RDF.Parsing;
+using VDS.RDF.Writing;
+using Vulsk.CarrePhrAggregator.DataSpecification;
 
 namespace Vulsk.CarrePhrAggregator.ResourceOutput
 {
@@ -22,6 +27,37 @@ namespace Vulsk.CarrePhrAggregator.ResourceOutput
 			var xdoc = new XmlDocument();
 			xdoc.Load(output);
 			return xdoc;
+		}
+
+		public IGraph ToRdf(List<PhrData> input)
+		{
+			IGraph g = new Graph();
+			var carreNode = g.CreateUriNode(UriFactory.Create("http://carre-project.eu"));
+			foreach (var i in input)
+			{
+				foreach (var d in i.Data) {
+					var predicate = g.CreateUriNode(UriFactory.Create(string.Format("http://carre-project.eu/{0}", d.Name)));
+					var value = g.CreateLiteralNode(d.Value.ToString());
+					g.Assert(new Triple(carreNode, predicate, value));
+				}
+			}
+
+			return g;
+		}
+
+		public void WriteRdf(IGraph graph, string fileName)
+		{
+			var path = Path.GetFullPath(Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().CodeBase.Replace(@"file:///", ""))));
+			var rdfxmlwriter = new RdfXmlWriter();
+			rdfxmlwriter.Save(graph, path + "/" + fileName + ".rdf");
+		}
+
+		public IGraph ReadRdf(string pathToFile)
+		{
+			IGraph g = new Graph();
+			FileLoader.Load(g, pathToFile);
+
+			return g;
 		}
 	}
 }
